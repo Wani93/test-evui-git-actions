aca389ec699920cf18d0426c78603e6ad74b5c8a
Wani <devwani93@gmail.com>
2022-06-08 11:01:50 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/test-evui-git-actions/' : '/',
+  publicPath: '/test-evui-git-actions/',
   outputDir: 'dist',
   devServer: {
     overlay: false,
